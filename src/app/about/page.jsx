115bdfc9a2f78854e3cc4dfc45ed25f631d70d46
Darli <n00167313@upn.pe>
2023-12-14 12:39:54 -0500

    Refactor code to improve performance and readability

---
+++ src/app/about/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AboutDescription from './components/AboutDescription';
+
+function page() {
+  return (
+    <>
+      <AboutDescription />
+    </>
+  );
+}
+
+export default page
