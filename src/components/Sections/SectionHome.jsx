b1dc49dd8f43e43886f7c36c49a991a848b218ee
Darli <n00167313@upn.pe>
2023-12-14 16:08:20 -0500

    Update image styling in SectionHome component

--- src/components/Sections/SectionHome.jsx
+++ src/components/Sections/SectionHome.jsx
@@ -12,7 +12,7 @@ function SectionHome() {
       >
         <div>
         <img
-            className="hidden mt-2 max-[320px]:mb-4 max-[425px]:mt-6 max-[320px]:mt-2 w-24 h-24 max-[320px]:w-14 max-[320px]:h-14 rounded-full shadow-lg max-[425px]:flex"
+            className="hidden mt-2 max-[320px]:mb-4 max-[425px]:mt-4 max-[320px]:mt-2 w-24 h-24 max-[320px]:w-14 max-[320px]:h-14 rounded-full shadow-lg max-[425px]:flex"
             src="https://i.imgur.com/ITW6pUq.jpg"
             alt=""
           />
