094df16c9a98973c64a2e70ed1568afc7d72addb
Darli <n00167313@upn.pe>
2023-12-14 14:03:09 -0500

    Update text size and spacing in DescriptionHome component

--- src/components/Sections/components/DescriptionHome.jsx
+++ src/components/Sections/components/DescriptionHome.jsx
@@ -14,7 +14,7 @@ function DescriptionHome() {
       </h1>
       <DescriptionOne />
 
-      <p className="text-gray-500  text-[16px]  leading-6 w-[85%] mt-4 max-[1024px]:text-[14px] max-[768px]:text-[12px] max-[768px]:mt-1 max-[425px]:text-[12px] max-[425px]:leading-normal max-[425px]:text-justify max-[425px]:w-[89%] ">
+      <p className="text-gray-500  text-[16px]  leading-6 w-[85%] mt-4 max-[1024px]:text-[14px] max-[768px]:text-[16px] max-[768px]:mt-1 max-[425px]:text-[12px] max-[425px]:leading-normal max-[425px]:text-justify max-[425px]:w-[89%] ">
       ¡Hola! Soy un ingeniero civil apasionado por la programación, combinando lo mejor de ambos mundos en mi carrera, apasionado por la investigación y por combinar mis habilidades para resolver problemas en la industria de la construcción. Tengo sólidos conocimientos en gestión de proyectos, diseño estructural y arquitectónico. En el ámbito de la programación, soy un desarrollador web front-end full stack, con experiencia en tecnologías como HTML, CSS, JS, React, Next.js, Framer Motion, Tailwind CSS y SQLite. Mi portafolio refleja mi pasión y evolución desde mis primeros cursos hasta convertirlo en un estilo de vida.
       <Link href="/about">
       <ButtonTwo text="¡Explora más aquí¡"></ButtonTwo>
